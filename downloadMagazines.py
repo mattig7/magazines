3291e070615dc70bb3e3a5e7e625259ba2d68770
Matt Graham <matt_graham2001@hotmail.com>
2020-08-04 16:50:30 +1000

    Restructured the program, logging and json config
    Lay out the structure of the program by creating functions for the main
    logic and functionality of finding links for downloading from the site.
    Added in logging capability - both filehandler and stream handler.
    Added in loading of magazine download configurations from a default
    configuration file. If this file doesn't exist, the program will create
    one with some default magazines to download.

--- downloadMagazines.py
+++ downloadMagazines.py
@@ -1,43 +1,74 @@
 #! python3
-# downloadXkcd.py - Downloads the first 35 hackspace magazines.
+"""
+downloadMagazines.py - Searches for and downloads magazines from specified sites.
+Sites and magazine names are specified in a default json file, but the script will
+ create one if it doesn't exist (so that you can change it and run the script again).
+TODO: Fix the header. Add comments. Add json file for magazines. Add a function for downloading the site, then a function for downloading an actual pdf. Insert logging commands.
+"""
 
-import requests, bs4, os, logging
+import requests, bs4 
+import os, logging, json, re
+from pathlib import Path
 
-magUrls = {'hackspace': 'https://hackspace.raspberrypi.org/issues/','magpi': 'https://magpi.raspberrypi.org/issues/'}
-for entry in magUrls.Items():
-   os.makedirs(entry, exist_ok=True)
+desiredLogLevel = logging.DEBUG
+logger = logging.getLogger('Logger for magazine download program')
+logger.setLevel(desiredLogLevel) #Pass all message levels to the handlers by default
 
-# Download the url, and find all instances of magazine download links
+logFormatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+streamHandler.setFormatter(logFormatter)
 
+logger.addHandler(streamHandler)
+logger.debug("Commenced initialiastion of logger and necessary parameters")
 
-for issuenum in range(9,32):
-   # Download the magazine.
-   magUrl = url + str(issuenum) + '/pdf'
-   print(f'Downloading issue {issuenum} from {magUrl}')
-   res = requests.get(magUrl)
-   res.raise_for_status()
-   
-   soup = bs4.BeautifulSoup(res.text, 'html.parser')
-   magElements = soup.select('p a.c-link')
-   
-   if magElements == []:
-      print('Could not find magazine.')
-   else:
-      actualMagUrl = magElements[0].get('href')
-      print(f'Saving magazine from actual link {actualMagUrl}')
-      
-      magFile = open(os.path.join('hackspace', str(issuenum) + '.pdf'), 'wb')
-      res2 = requests.get(actualMagUrl)
-      res2.raise_for_status()
+magDefsFile = Path("./mag_config.json")
+logFile = Path("./mag.log")
 
-      for chunk in res2.iter_content(100000):
-         magFile.write(chunk)
-      
-      magFile.close()
+logger.debug("Creating file handler for logging")
 
-print('Done.')
+fileHandler = logging.FileHandler(logFile,mode='w') #overwrites previous log
+fileHandler.setLevel(desiredLogLevel)
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+logger.debug(f"Completed creating the filehandler for logging using file {logFile}")
+logger.debug("Completed establishing logging capability")
 
 
 
+def findAndDownloadLinks (URL, magName):
+    """
+    Function to search through a website and download all potential magazine links it finds.
+    Will call itself recursively on subsites that look promising to try to find all the magazines present.
+    """
+    logger.debug("Commenced 'findAndDownloadLinks' function for {magName} with URL: {URL}.")
+    
+    logger.debug("Completed 'findAndDownloadLinks' function for {magName} with URL: {URL}.")
 
+if __name__ == "__main__":
+    """
+    main function. Creates the magazine config file if not present, loads it and commences
+    the program otherwise.
+    """
+    logger.debug("Commencing main function")
+    magUrls = ""
+    #If magazine definitions file exists, load it. Otherwise make one
+    if not magDefsFile.exists():
+        logger.debug(f"Magazine definitions file, {magDefsFile}, does not exist. Creating it")
+        magUrls = {'hackspace': 'https://hackspace.raspberrypi.org/issues/','magpi': 'https://magpi.raspberrypi.org/issues/'}
+        with open(magDefsFile, 'w') as outfile:
+            json.dump(magUrls, outfile, indent=4)
+        logger.debug(f"Finished creating magazine definitions file {magDefsFile} with data:\n{magUrls}")
+    else:
+        logger.debug(f"Found magazine definition file at {magDefsFile}. Loading data")
+        with open(magDefsFile) as infile:
+            magUrls = json.load(infile)
+    logger.debug(f"Established magazine definitions as:\n{str(magUrls)}")
+
+    # Call the functions to start the capability.
+
+
+
+    logger.debug(f"Completed main function")
