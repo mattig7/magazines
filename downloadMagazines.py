06483477cc93dd15f7fc33007c25ddeb111a6f4a
Matt Graham <matt_graham2001@hotmail.com>
2020-08-05 19:35:45 +1000

    Completed version 1.0
    Finished up the downloading logic for the program, then tidied up the
    code a little. As far as I can tell the program is working perfectly. So
    I'll release this for Beta testing!

--- downloadMagazines.py
+++ downloadMagazines.py
@@ -52,12 +52,12 @@ def downloadLink(URL, magname):
     """
     Function to download a magazine that has been found.
     """
-    logger.debug(f"Commencing function 'downloadLink' with:\nURL: {URL}\nmagname: {magname}")
+    logger.info(f"Commencing function 'downloadLink' with:\nURL: {URL}\nmagname: {magname}")
     if not ".pdf" in URL:
         logger.debug(f"downloadLink called for a URL that does not contain a pdf file: {URL}. Returning without doing anything.")
         return
     
-    Path("./magname").mkdir(exist_ok=True)
+    Path(f"./{magname}").mkdir(exist_ok=True)
     
     #Pull the filename out of the URL
     urlParsed = urlparse(URL)
@@ -71,9 +71,18 @@ def downloadLink(URL, magname):
 
     else:
         logger.debug(f"The pdf file {pdfFileName} doesn't exist. Downloading")
-    # Download the file if doesn't exist
-     
-    
+        try:
+            res = requests.get(URL)
+            res.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            logger.error(f"There was a problem with downloading: {pdfFileName}:\n {e}")
+            return
+        logger.debug(f"Opening file for writing: {pdfFileName}")
+        f = open(pdfFileName, 'wb')
+        logger.debug(f"Iterating through the request to download file")
+        for chunk in res.iter_content(100000):
+            f.write(chunk)
+
     logger.debug(f"Completed function 'downloadLink' with:\nURL: {URL}\nmagname: {magname}")
 
 def findAndDownloadLinks (URL, magName):
@@ -82,7 +91,7 @@ def findAndDownloadLinks (URL, magName):
     Will call itself recursively on subsites that look promising to try to find all the magazines present.
     """
     regex_GoodLink = re.compile(r"(download|pdf|next)", flags=re.I)
-    logger.debug(f"Commenced 'findAndDownloadLinks' function for {magName} with URL: {URL}.")
+    logger.info(f"Commenced 'findAndDownloadLinks' function for {magName} with URL: {URL}.")
     logger.debug(f"The regex to match for a good link text is {regex_GoodLink}")
     # send any pdf file links straight to downloadLink function
     if ".pdf" in URL:
@@ -100,7 +109,6 @@ def findAndDownloadLinks (URL, magName):
     
     # Use Beautiful soup to find the links of interest
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    #href = "href"
     #logger.debug(f"The site obtained from {URL} is:\n{soup.prettify()}")
     #logger.debug(f"The site ({URL}) main section is: \n{soup.body.main.prettify()}")
     for link in soup.body.main.select('a[href]'):
@@ -111,7 +119,7 @@ def findAndDownloadLinks (URL, magName):
             logger.debug(f"Attempting to join:\n{URL}\n{urlParsed.geturl()}") 
             urlParsed = urlparse(urljoin(URL, urlParsed.geturl()))
             logger.debug(f"Updated the relative href to be: {urlParsed.geturl()}")
-        #logger.debug(f"Testing for good links. \nText is: {link.get_text()}\nURL is {urlParsed.geturl()}")
+        # Test for and follow promising links
         if ".pdf" in urlParsed.geturl():
             logger.debug(f"The phrase '.pdf' is in the link. Calling downloadLink method to get it!")
             downloadLink(urlParsed.geturl(), magname)
@@ -121,19 +129,17 @@ def findAndDownloadLinks (URL, magName):
             findAndDownloadLinks(urlParsed.geturl(), magname)
         else:
             logger.debug(f"No PDF here and no good words. No action for this link")
-            #logger.debug(f"For reference, output of the regex match is {regex_GoodLink.match(link.get_text())}\nAnd the output of testing if urls are different is: {urlParsed.geturl() != URL}\nTherefore the if test should be: {bool(regex_GoodLink.match(link.get_text()) and urlParsed.geturl() != URL)}")
     logger.debug(f"Completed 'findAndDownloadLinks' function for {magName} with URL: {URL}.")
 
 if __name__ == "__main__":
     """
-    main function. Creates the magazine config file if not present, loads it and commences
-    the program otherwise.
+    main function. Creates the magazine config file if not present, loads it and commences the program otherwise.
     """
-    logger.debug("Commencing main function")
+    logger.info("Commencing main function")
     magUrls = ""
     #If magazine definitions file exists, load it. Otherwise make one
     if not magDefsFile.exists():
-        logger.debug(f"Magazine definitions file, {magDefsFile}, does not exist. Creating it")
+        logger.warning(f"Magazine definitions file, {magDefsFile}, does not exist. Creating it")
         magUrls = {'hackspace': 'https://hackspace.raspberrypi.org/issues/','magpi': 'https://magpi.raspberrypi.org/issues/'}
         with open(magDefsFile, 'w') as outfile:
             json.dump(magUrls, outfile, indent=4)
@@ -142,11 +148,11 @@ if __name__ == "__main__":
         logger.debug(f"Found magazine definition file at {magDefsFile}. Loading data")
         with open(magDefsFile) as infile:
             magUrls = json.load(infile)
-    logger.debug(f"Established magazine definitions as:\n{str(magUrls)}")
+    logger.info(f"Established magazine definitions as:\n{str(magUrls)}")
     
     # Call the functions to start the capability.
     for magname, URL in magUrls.items():
         logger.debug(f"Commencing downloading of {magname} for URL {URL}")
         findAndDownloadLinks(URL, magname)
     
-    logger.debug(f"Completed main function")
+    logger.info(f"Completed main function")
